14c93ae94fa000e8773cdec3322d0fbe20574dba
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2022-12-07 21:30:55 +0100

    day 7 mild refactor

--- day7.py
+++ day7.py
@@ -1,45 +1,30 @@
-from collections import deque
-
-from anytree import Node, Walker, RenderTree, PostOrderIter
+from anytree import Node, RenderTree, PostOrderIter
 
 
-#
-#
-def algo() -> int:
-    print("hello, day 7: No Space Left On Device")
-
-    commands = open("day7_input.txt").readlines()
-
+def create_tree_from(commands) -> Node:
     root: Node = Node('root', directory_size=int(0), type='dir')
     current_dir: Node = root
 
     all_nodes_by_name = dict()
     all_nodes_by_name['root'] = root
-    nodes_stack = deque()
-    nodes_stack.append(root)
-    w = Walker()
 
     for cmd_raw in commands:
         cmd = cmd_raw.strip()
         match cmd:
             case '$ cd /':
                 # print("cd root")
-                root = nodes_stack.pop()
-                w.walk(root, root)
                 current_dir = root
             case '$ ls':
                 print("ls")
                 # ignore
             case '$ cd ..':
                 parent = current_dir.parent
-                w.walk(current_dir, parent)
                 current_dir = parent
             case _:
                 if cmd.startswith('$ cd'):
                     _, _, target_dir_name = cmd.split(' ')
                     # print(f'cd {target_dir_name}')
                     target_dir = all_nodes_by_name.get(target_dir_name)
-                    w.walk(current_dir, target_dir)
                     current_dir = target_dir
                 elif cmd.startswith('dir'):
                     _, dir_name = cmd.split(' ')
@@ -47,7 +32,6 @@ def algo() -> int:
                     new_dir = Node(dir_name, parent=current_dir, directory_size=int(0), type='dir')
 
                     all_nodes_by_name[dir_name] = new_dir
-                    nodes_stack.append(new_dir)
                 else:
                     file_size, file_name = cmd.split(' ')
                     # print(f' file creation {file_name}')
@@ -57,8 +41,10 @@ def algo() -> int:
                     t = new_file.__getattribute__('type')
                     # print(f'file size {s} {t}')
                     all_nodes_by_name[file_name] = new_file
-                    nodes_stack.append(new_file)
+    return root
 
+
+def visualize_tree(root: Node):
     print('--------')
     for pre, fill, node in RenderTree(root):
         print("%s%s" % (pre, node.name))
@@ -66,6 +52,8 @@ def algo() -> int:
     all_directories = PostOrderIter(root, filter_=lambda n: n.__getattribute__('type') == 'dir')
     print([directory.name for directory in all_directories])
 
+
+def calculate_size_of_each_directory(root: Node):
     print('--------')
     print("let's calculate the size of each directory")
     for dir_node in PostOrderIter(root, filter_=lambda n: n.type == 'dir'):
@@ -81,6 +69,8 @@ def algo() -> int:
         dir_node.__setattr__('directory_size', total_dir_size)
         print(f'{dir_node.name}, size={dir_node.directory_size}')
 
+
+def sum_size_of_small_directories(root: Node):
     print('--- filter dirs with size smaller than 100000')
     result_dirs = PostOrderIter(root, filter_=lambda n: n.__getattribute__('type') == 'dir' and n.__getattribute__(
         'directory_size') <= 100000)
@@ -92,6 +82,15 @@ def algo() -> int:
         result = result + d.__getattribute__('directory_size')
 
     return result
+
+
+def algo() -> int:
+    print("hello, day 7: No Space Left On Device")
+    commands = open("day7_input.txt").readlines()
+    root = create_tree_from(commands)
+    visualize_tree(root)
+    calculate_size_of_each_directory(root)
+    return sum_size_of_small_directories(root)
 
 
 if __name__ == '__main__':
