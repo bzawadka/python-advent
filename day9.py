bc5f310303db5088d01894edafa6cb5bcd68afcd
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2022-12-09 14:39:47 +0100

    day 9 playing with python

--- day9.py
+++ day9.py
@@ -1,19 +1,70 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Instr:
+    cmd: str
+    step: int
+
+
 def algo() -> int:
-    print("hello, day 9: ")
-
-    lines_raw = open("day9_input.txt").readlines()
-    lines = [line.strip() for line in lines_raw]
-    print(lines)
-
-    cosik = [i*2 for i in range(10)]
-    print(cosik)
-
-    cos = [[0] * 10 for _ in range(10)]
-    print(cos)
-
-    result = 0
+    print("hello, day 9: Rope Bridge")
+    instructions = read_instructions()
+    result = calculate_how_many_positions_tail_visited(instructions)
     return result
 
 
+def read_instructions() -> list[Instr]:
+    instructions_raw = open("day9_test_input.txt").readlines()
+    instructions_lines = [line.strip() for line in instructions_raw]
+    instructions = [Instr(it.split(" ")[0], int(it.split(" ")[1])) for it in instructions_lines]
+    if debug:
+        print(instructions)
+    return instructions
+
+
+def calculate_how_many_positions_tail_visited(instructions) -> int:
+    # positions of head and tail
+    head_x = 0
+    head_y = 0
+
+    for instr in instructions:
+        match instr.cmd:
+            case 'R':
+                for i in range(0, instr.step):
+                    head_x += 1
+            case 'L':
+                for i in range(0, instr.step):
+                    head_x -= 1
+            case 'U':
+                for i in range(0, instr.step):
+                    head_y += 1
+            case 'D':
+                for i in range(0, instr.step):
+                    head_y -= 1
+            case _:
+                raise SyntaxError
+        print(f'head is [{head_x}][{head_y}]')
+
+        visualize_position(head_x, head_y, 'H')
+
+    return 0
+
+
+def visualize_position(x: int, y: int, character: str):
+    grid = [[0] * grid_size for _ in range(grid_size)]
+    # bottom left corner is [0][0]
+    grid[grid_size - 1 - y][x] = character
+    print('y')
+    print('^')
+    for line in grid:
+        print(f'| {line}')
+    print('o---------------------> x')
+    print()
+
+
 if __name__ == '__main__':
+    debug = True
+    grid_size = 6
+    # How many positions does the tail of the rope visit at least once?
     print(f'result: {algo()}')
