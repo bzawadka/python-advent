0265a4313e3fd1b718b0a5a55689be6dae018dcf
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2022-12-12 15:36:21 +0100

    day 9 - correct

--- day9.py
+++ day9.py
@@ -21,117 +21,61 @@ def read_instructions() -> list[Instr]:
     return instructions
 
 
-previous_head_direction = 'undefined'
-head_direction = 'undefined'
-head_changed_direction = False
-
-
 def calculate_how_many_positions_tail_visited(instructions) -> int:
     # positions of head and tail
     grid_size = 150
-    head_x = 75
-    head_y = 75
-    tail_x = 75
-    tail_y = 75
+    start_x = 75
+    start_y = 75
+    head_x = start_x
+    head_y = start_y
+    tail_x = start_x
+    tail_y = start_y
     positions_tail_visited_set = set()
 
     visualize_position(head_x, head_y, 'H', tail_x, tail_y, 'T', grid_size)
     mark_current_tail_position_as_visited(tail_x, tail_y, positions_tail_visited_set)
 
     for instr in instructions:
-        print(f' == {instr.cmd} {instr.step} ========================')
+        if trace:
+            print(f' == {instr.cmd} {instr.step} ===')
         match instr.cmd:
             case 'R':
                 for i in range(0, instr.step):
+                    previous_head_x = head_x
                     head_x += 1
-                    update_head_direction_to('horizontal')
 
-                    # in the first move
-                    if i == 0:
-                        # if head and tail were on the same position
-                        if head_y == tail_y and head_x - 1 == tail_x:
-                            print()  # do nothing
-                        # if head and tail were on diagonal after previous move
-                        elif tail_x + 2 == head_x:
-                            tail_x += 1
-                            if tail_y + 1 == head_y:
-                                tail_y += 1
-                            if head_y + 1 == tail_y:
-                                tail_y -= 1
-                        # if head and tail were next to each other, but...
-                        elif head_changed_direction:
-                            print()  # do nothing
-                        # if head just got closer to the tail - from diagonal to adjacent
-                        elif head_x == tail_x:
-                            print()  # do nothing
-                        else:
-                            # tail was already behind the head, direction stays - tail shall continue
-                            tail_x += 1
-                    # in the second move
-                    elif i == 1 or i == 2:
-                        # if head and tail are touching diagonally
-                        if tail_x + 1 == head_x:
-                            print()  # do nothing
-                        else:
-                            if not head_changed_direction:
-                                tail_x += 1
-                            else:
-                                # go right potentially diagonally - up or down
-                                tail_x += 1
-                                # print('if head is below tail, go right bottom with the tail')
-                                if head_y < tail_y:
-                                    tail_y -= 1
-                                elif head_y > tail_y:
-                                    tail_y += 1
-                    else:
+                    # tail moves along
+                    if head_y == tail_y and tail_x + 1 == previous_head_x:
                         tail_x += 1
+                    # tail moves diagonally
+                    elif tail_x + 1 == previous_head_x and (tail_y + 1 == head_y or head_y + 1 == tail_y):
+                        tail_x += 1
+                        if tail_y + 1 == head_y:
+                            tail_y += 1
+                        if head_y + 1 == tail_y:
+                            tail_y -= 1
+                    # otherwise don't move tail
+
                     mark_current_tail_position_as_visited(tail_x, tail_y, positions_tail_visited_set)
                     if trace:
                         visualize_position(head_x, head_y, 'H', tail_x, tail_y, 'T', grid_size)
 
             case 'L':
                 for i in range(0, instr.step):
+                    previous_head_x = head_x
                     head_x -= 1
-                    update_head_direction_to('horizontal')
 
-                    # in the first move
-                    if i == 0:
-                        # if head and tail were on the same position
-                        if head_y == tail_y and head_x + 1 == tail_x:
-                            print('')  # do nothing
-                        # if head and tail were on diagonal after previous move
-                        elif head_x + 2 == tail_x:
-                            tail_x -= 1
-                            if tail_y + 1 == head_y:
-                                tail_y += 1
-                            if head_y + 1 == tail_x:
-                                tail_y -= 1
-                        # if head and tail were next to each other, but...
-                        elif head_changed_direction:
-                            print()  # do nothing
-                        # if head just got closer to the tail - from diagonal to adjacent
-                        elif head_x == tail_x:
-                            print()  # do nothing
-                        else:
-                            # tail was already behind the head, direction stays - tail shall continue
-                            tail_x -= 1
-                    # in the second move
-                    elif i == 1 or i == 2:
-                        if head_x + 1 == tail_x:
-                            print()  # do nothing
-                        else:
-                            if not head_changed_direction:
-                                tail_x -= 1
-                            else:
-                                # go left potentially diagonally - up or down
-                                tail_x -= 1
-                                # print('if head is below tail, go left bottom with the tail')
-                                if head_y < tail_y:
-                                    tail_y -= 1
-                                elif head_y > tail_y:
-                                    tail_y += 1
-                    else:
+                    # tail moves along
+                    if head_y == tail_y and tail_x - 1 == previous_head_x:
                         tail_x -= 1
+                    # tail moves diagonally
+                    elif tail_x - 1 == previous_head_x and (tail_y + 1 == head_y or head_y + 1 == tail_y):
+                        tail_x -= 1
+                        if tail_y + 1 == head_y:
+                            tail_y += 1
+                        if head_y + 1 == tail_y:
+                            tail_y -= 1
+                    # otherwise don't move tail
 
                     mark_current_tail_position_as_visited(tail_x, tail_y, positions_tail_visited_set)
                     if trace:
@@ -139,46 +83,20 @@ def calculate_how_many_positions_tail_visited(instructions) -> int:
 
             case 'U':
                 for i in range(0, instr.step):
+                    previous_head_y = head_y
                     head_y += 1
-                    update_head_direction_to('vertical')
 
-                    # in the first move
-                    if i == 0:
-                        # if head and tail were on the same position
-                        if head_y - 1 == tail_y and head_x == tail_x:
-                            print()  # do nothing
-                        # if head and tail were on diagonal after previous move
-                        elif tail_y + 2 == head_y:
-                            tail_y += 1
-                            if tail_x + 1 == head_x:
-                                tail_x += 1
-                            if head_x + 1 == tail_x:
-                                tail_x -= 1
-                        elif head_changed_direction:
-                            print()  # do nothing
-                        # if head just got closer to the tail - from diagonal to adjacent
-                        elif head_y == tail_y:
-                            print()  # do nothing
-                        else:
-                            # tail was already behind the head, direction stays - tail shall continue
-                            tail_y += 1
-                    # in the second move
-                    elif i == 1 or i == 2:
-                        if head_y - 1 == tail_y:
-                            print()  # do nothing
-                        else:
-                            if not head_changed_direction:
-                                tail_y += 1
-                            else:
-                                # go up diagonally - right or left
-                                tail_y += 1
-                                # print('if head is left from tail, go up left with the tail')
-                                if head_x < tail_x:
-                                    tail_x -= 1
-                                elif head_x > tail_x:
-                                    tail_x += 1
-                    else:
+                    # tail moves along
+                    if head_x == tail_x and tail_y + 1 == previous_head_y:
                         tail_y += 1
+                    # tail moves diagonally
+                    if tail_y + 1 == previous_head_y and (tail_x + 1 == head_x or tail_x - 1 == head_x):
+                        tail_y += 1
+                        if tail_x + 1 == head_x:
+                            tail_x += 1
+                        if tail_x - 1 == head_x:
+                            tail_x -= 1
+                    # otherwise don't move tail
 
                     mark_current_tail_position_as_visited(tail_x, tail_y, positions_tail_visited_set)
                     if trace:
@@ -186,45 +104,19 @@ def calculate_how_many_positions_tail_visited(instructions) -> int:
 
             case 'D':
                 for i in range(0, instr.step):
+                    previous_head_y = head_y
                     head_y -= 1
-                    update_head_direction_to('vertical')
 
-                    # in the first move
-                    if i == 0:
-                        # if head and tail were on the same position
-                        if head_y + 1 == tail_y and head_x == tail_x:
-                            print()  # do nothing
-                        # if head and tail were on diagonal after previous move
-                        elif head_y + 2 == tail_y:
-                            tail_y -= 1
-                            if tail_x + 1 == head_x:
-                                tail_x += 1
-                            if head_x + 1 == tail_x:
-                                tail_x -= 1
-                        elif head_changed_direction:  # POTENTIAL PROBLEM!!
-                            print()  # do nothing
-                        # if head just got closer to the tail - from diagonal to adjacent
-                        elif head_y == tail_y:
-                            print()  # do nothing
-                        else:
-                            # tail was already behind the head, direction stays - tail shall continue
-                            tail_y -= 1
-                    # in the second move
-                    elif i == 1 or i == 2:
-                        if head_y + 1 == tail_y:
-                            print()  # do nothing
-                        else:
-                            if not head_changed_direction:
-                                tail_y -= 1
-                            else:
-                                # go down diagonally - right or left
-                                tail_y -= 1
-                                if head_x > tail_x:
-                                    tail_x += 1
-                                elif head_x < tail_x:
-                                    tail_x -= 1
-                    else:
+                    # tail moves along
+                    if head_x == tail_x and tail_y - 1 == previous_head_y:
                         tail_y -= 1
+                    # tail moves diagonally
+                    if tail_y - 1 == previous_head_y and (tail_x + 1 == head_x or tail_x - 1 == head_x):
+                        tail_y -= 1
+                        if tail_x + 1 == head_x:
+                            tail_x += 1
+                        if tail_x - 1 == head_x:
+                            tail_x -= 1
 
                     mark_current_tail_position_as_visited(tail_x, tail_y, positions_tail_visited_set)
                     if trace:
@@ -244,20 +136,8 @@ def calculate_how_many_positions_tail_visited(instructions) -> int:
     return len(positions_tail_visited_set)
 
 
-def update_head_direction_to(direction: str):
-    global previous_head_direction
-    global head_direction
-    global head_changed_direction
-
-    previous_head_direction = head_direction
-    head_direction = direction
-    if previous_head_direction != 'undefined' and head_direction != previous_head_direction:
-        head_changed_direction = True
-        # print(f'what a twist of direction from {previous_head_direction} to {direction}!')
-
-
 def mark_current_tail_position_as_visited(tail_x, tail_y, positions_tail_visited_set):
-    item = str.format('{}{}', tail_x, tail_y)
+    item = str.format('{}_{}', tail_x, tail_y)
     positions_tail_visited_set.add(item)
 
 
@@ -285,6 +165,9 @@ if __name__ == '__main__':
     # How many positions does the tail of the rope visit at least once?
     print(f'result: {algo()}')
     # 6428 your answer is too high
+
+    # 6376 - That's the right answer!
+
     # 6374 - that's not the right answer
     # 6377 That's not the right answer
     # 6355, 6356 - your answer is too low.
