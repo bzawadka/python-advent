b6903349640e11376ebb817caf7936aeeda47d82
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-12-01 12:18:31 +0100

    day 1 cleanup

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,21 +1,36 @@
-def algo_part_one(input_file_name) -> int:
-    lines = read_file_to_list(input_file_name)
+import re
 
+
+def algo_part_one(input_file_name) -> int:
+    print("running algo part one..." + input_file_name)
+    lines = read_file_to_list(input_file_name)
     sum_val = 0
     for line in lines:
         first_d = find_first_digit(line)
         last_d = find_first_digit(line[::-1])
         number = int(first_d) * 10 + int(last_d)
         sum_val += number
-        # digits = re.findall(r'\d')
-
     return sum_val
 
 
 def find_first_digit(line):
-    for c in line:
-        if c.isdigit():
-            return int(c)
+    digits = re.findall(r'\d', line)
+    return int(digits[0])
+
+
+def algo_part_two(input_file_name) -> int:
+    print("running algo part two..." + input_file_name)
+
+    lines = read_file_to_list(input_file_name)
+
+    sum_val = 0
+    for line in lines:
+        first_d = find_first_digit_or_str(line)
+        last_d = find_first_digit_or_str_reversed(line[::-1])
+        number = int(first_d) * 10 + int(last_d)
+        sum_val += number
+
+    return sum_val
 
 
 def find_first_digit_or_str(line):
@@ -68,21 +83,6 @@ def find_first_digit_or_str_reversed(line):
                 return 9
 
 
-def algo_part_two(input_file_name) -> int:
-    print("running algo..." + input_file_name)
-
-    lines = read_file_to_list(input_file_name)
-
-    sum_val = 0
-    for line in lines:
-        first_d = find_first_digit_or_str(line)
-        last_d = find_first_digit_or_str_reversed(line[::-1])
-        number = int(first_d) * 10 + int(last_d)
-        sum_val += number
-
-    return sum_val
-
-
 def read_file_to_list(file) -> list[str]:
     instructions_raw = open(file).readlines()
     instructions_lines = [line.strip() for line in instructions_raw]
@@ -94,8 +94,13 @@ if __name__ == '__main__':
     debug = False
     trace = False
     test_input_file = f'input/day{day}/testInput.txt'
+    test_input_file_p1 = f'input/day{day}/testInput_p1.txt'
     input_file = f'input/day{day}/input.txt'
 
+    assert 142 == algo_part_one(test_input_file_p1)
+    assert 55607 == algo_part_one(input_file)
+
+    # part 2
     assert 1 == find_first_digit_or_str('abcone2threexyz')
     assert 4 == find_first_digit_or_str('4nineeightseven2')
     assert 2 == find_first_digit_or_str('xtwone3four')
@@ -104,8 +109,6 @@ if __name__ == '__main__':
     assert 2 == find_first_digit_or_str_reversed('4nineeightseven2'[::-1])
     assert 6 == find_first_digit_or_str_reversed('7pqrstsixteen'[::-1])
 
-    # run with test file and expect given result
     assert 281 == algo_part_two(test_input_file)
-
-    # run with non-test file": 1, "and print the result
     print(f'result: {algo_part_two(input_file)}')
+    assert 55291 == algo_part_two(input_file)
