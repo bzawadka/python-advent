a1ed2e383a7dad960ace58a0458b982092274e38
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-12-03 23:01:10 +0100

    day3 part 2

--- 2023/day3.py
+++ 2023/day3.py
@@ -18,78 +18,137 @@ def algo_part_one(input_file_name) -> int:
         for part_number_match in re.finditer(r"\d+", row):
             start_idx = part_number_match.start()
             end_idx = part_number_match.end()
-            part_number = row[start_idx:end_idx]
-            # print(f'part number: {part_number}')
+            part_number = int(row[start_idx:end_idx])
 
             # collect all characters around this number
-            # if one of those  characters is a symbol
+            # if one of those characters is a symbol
             # then add part number to valid part numbers set
-            chars_around = set()
-            # before index
+            neighbours = set()
             if start_idx > 0:
                 preceding_char = row[start_idx - 1: start_idx]
-                chars_around.add(preceding_char)
-            # after index
+                neighbours.add(preceding_char)
             if end_idx + 1 < len(row):
                 succeeding_char = row[end_idx: end_idx + 1]
-                chars_around.add(succeeding_char)
-            # below
+                neighbours.add(succeeding_char)
             if y + 1 < len(grid):
-                next_row = grid[y + 1]
-                row_part_below = next_row[start_idx:end_idx]
-                chars_around = chars_around | set(row_part_below)
-            # above
+                row_below = grid[y + 1]
+                neighbours = neighbours | set(row_below[start_idx:end_idx])
             if y > 0:
-                previous_row = grid[y - 1]
-                row_part_above = previous_row[start_idx:end_idx]
-                chars_around = chars_around | set(row_part_above)
-            # and diagonal, bottom right
+                row_above = grid[y - 1]
+                neighbours = neighbours | set(row_above[start_idx:end_idx])
+            # diagonal, bottom right
             if y + 1 < len(grid) and end_idx + 1 < len(row):
-                a = grid[y + 1][end_idx]
-                chars_around.add(a)
-            # and diagonal, bottom left
+                neighbours.add(grid[y + 1][end_idx])
+            # diagonal, bottom left
             if y + 1 < len(grid) and start_idx > 0:
-                a = grid[y + 1][start_idx - 1]
-                chars_around.add(a)
-            # and diagonal, top right
+                neighbours.add(grid[y + 1][start_idx - 1])
+            # diagonal, top right
             if y > 0 and end_idx + 1 < len(row):
-                a = grid[y - 1][end_idx]
-                chars_around.add(a)
-            # and diagonal, top left
+                neighbours.add(grid[y - 1][end_idx])
+            # diagonal, top left
             if y > 0 and start_idx > 0:
-                a = grid[y - 1][start_idx - 1]
-                chars_around.add(a)
+                neighbours.add(grid[y - 1][start_idx - 1])
 
-            for ca in chars_around:
-                if ca in symbols:
-                    valid_part_numbers.append(int(part_number))
-                    print(f'valid part: {part_number}')
-
-        # for y, col in enumerate(row):
-        #     c = grid[x][y]
-        #     if c in symbols:
-        #         print(f'symbol: {c}')
+            for n in neighbours:
+                if n in symbols:
+                    valid_part_numbers.append(part_number)
 
     return sum(valid_part_numbers)
 
 
+# A gear is any * symbol that is adjacent to exactly two part numbers
+# Its gear ratio is the result of multiplying those two numbers together
 def algo_part_two(input_file_name) -> int:
     print("running algo part two..." + input_file_name)
-    return algo_part_one(input_file_name)
+    grid = read_file_to_list(input_file_name)
+
+    gear_symbol = '*'
+    for y, row in enumerate(grid):
+
+        for part_number_match in re.finditer(r"\d+", row):
+            start_idx = part_number_match.start()
+            end_idx = part_number_match.end()
+            part_number = int(row[start_idx:end_idx])
+
+            # collect all gears with all adjacent part numbers
+            # preceding char
+            if start_idx > 0:
+                check_for_gear_symbol_at(y, start_idx - 1, grid, part_number)
+
+            # succeeding char
+            if end_idx + 1 < len(row):
+                check_for_gear_symbol_at(y, end_idx, grid, part_number)
+
+            if y + 1 < len(grid):
+                row_below = grid[y + 1]
+                row_part_below = row_below[start_idx:end_idx]
+                # find out exact position of *
+                if gear_symbol in row_part_below:
+                    offset = row_part_below.find(gear_symbol)
+                    key = str(y + 1) + '_' + str(start_idx + offset)
+                    update_gear_map(key, part_number)
+
+            if y > 0:
+                row_above = grid[y - 1]
+                row_part_above = row_above[start_idx:end_idx]
+                # find out exact position of *
+                if gear_symbol in row_part_above:
+                    offset = row_part_above.find(gear_symbol)
+                    key = str(y - 1) + '_' + str(start_idx + offset)
+                    update_gear_map(key, part_number)
+
+            # diagonal, bottom right
+            if y + 1 < len(grid) and end_idx + 1 < len(row):
+                check_for_gear_symbol_at(y + 1, end_idx, grid, part_number)
+
+            # diagonal, bottom left
+            if y + 1 < len(grid) and start_idx > 0:
+                check_for_gear_symbol_at(y + 1, start_idx - 1, grid, part_number)
+
+            # diagonal, top right
+            if y > 0 and end_idx + 1 < len(row):
+                check_for_gear_symbol_at(y - 1, end_idx, grid, part_number)
+
+            # diagonal, top left
+            if y > 0 and start_idx > 0:
+                check_for_gear_symbol_at(y - 1, start_idx - 1, grid, part_number)
+
+    # PHASE 2
+    gear_ratios = list()
+    for key, part_numbers in gears_with_part_numbers.items():
+        # valid gear is adjacent to exactly two part numbers
+        # Its gear ratio is the result of multiplying those two numbers together
+        if len(part_numbers) == 2:
+            gear_ratios.append(part_numbers[0] * part_numbers[1])
+
+    return sum(gear_ratios)
+
+
+def check_for_gear_symbol_at(y, x, grid, part_number):
+    if grid[y][x] == '*':
+        key = str(y) + '_' + str(x)
+        update_gear_map(key, part_number)
+
+
+def update_gear_map(key: str, part_number: int):
+    if not gears_with_part_numbers.get(key):
+        gears_with_part_numbers[key] = [part_number]
+    else:
+        parts = gears_with_part_numbers.get(key)
+        parts.append(part_number)
+        gears_with_part_numbers[key] = parts
 
 
 if __name__ == '__main__':
     day = 3
-    debug = False
-    trace = False
     test_input_file = f'input/day{day}/testInput.txt'
     input_file = f'input/day{day}/input.txt'
 
-    # assert 4361 == algo_part_one(test_input_file)
-    print(f'result: {algo_part_one(input_file)}')
-    # assert 42 == algo_part_one(input_file)
-    # 328113
+    assert 4361 == algo_part_one(test_input_file)
+    # print(f'result: {algo_part_one(input_file)}')
+    assert 532331 == algo_part_one(input_file)
     # part 2
-    # assert 42 == algo_part_two(test_input_file)
-    # print(f'result: {algo_part_two(input_file)}')
-    # assert 42 == algo_part_two(input_file)
+    gears_with_part_numbers = dict()
+    assert 467835 == algo_part_two(test_input_file)
+    gears_with_part_numbers.clear()
+    assert 82301120 == algo_part_two(input_file)
