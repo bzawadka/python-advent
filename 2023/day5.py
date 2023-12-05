9fd768b080de56606ff5e4b421697f7dfbd3aa8c
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-12-05 21:49:14 +0100

    day 5 overall cleanup

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,35 +1,22 @@
 import re
 
 
+# --- Day 5: If You Give A Seed A Fertilizer ---
 def algo_part_one(input_file_name) -> int:
     print("running algo part one..." + input_file_name)
-    lines_raw = open(input_file_name).readlines()
-    seeds = re.findall(r'\d+', lines_raw[0])
-
-    # parse input
-    maps = [[], [], [], [], [], [], []]
-    map_idx = -1
-    for line in lines_raw[1:]:
-        if line.__contains__('map'):
-            map_idx += 1
-        else:
-            numbers = re.findall(r'\d+', line)
-            if numbers:
-                maps[map_idx].append(numbers)
+    input_lines = open(input_file_name).readlines()
+    seeds = re.findall(r'\d+', input_lines[0])
+    maps = extract_maps(input_lines)
 
     for m in maps:
-        seed_already_updated = [False for _ in range(0, len(seeds))]
         for s_id, s in enumerate(seeds):
-            if seed_already_updated[s_id]:
-                break
-
             for map_line in m:
                 range_from = int(map_line[1])
                 range_length = int(map_line[2])
                 offset = int(map_line[0]) - range_from
                 if seed_in_range(int(s), range_from, range_length):
                     seeds[s_id] = int(s) + offset
-                    seed_already_updated[s_id] = True
+                    break
 
     # the lowest location number that corresponds to any of the initial seeds
     result = min(seeds)
@@ -43,37 +30,20 @@ def seed_in_range(seed: int, range_from: int, length: int):
 
 def algo_part_two(input_file_name) -> int:
     print("running algo part two..." + input_file_name)
-    lines_raw = open(input_file_name).readlines()
-    seeds_with_ranges = re.findall(r'\d+', lines_raw[0])
+    input_lines = open(input_file_name).readlines()
+    seeds_with_ranges = re.findall(r'\d+', input_lines[0])
     seeds = seeds_with_ranges[0::2]
     ranges = seeds_with_ranges[1::2]
-
-    actual_unique_seeds = set()
-    for s_idx, s in enumerate(seeds):
-        r = int(ranges[s_idx])
-        for i in range(0, r):
-            actual_seed = int(s) + i
-            actual_unique_seeds.add(actual_seed)
-
-    actual_seeds = list(actual_unique_seeds)
-    print(f'number of actual seeds: {len(actual_seeds)}')
-
-    # parse input
-    maps = [[], [], [], [], [], [], []]
-    map_idx = -1
-    for line in lines_raw[1:]:
-        if line.__contains__('map'):
-            map_idx += 1
-        else:
-            numbers = re.findall(r'\d+', line)
-            if numbers:
-                maps[map_idx].append(numbers)
+    maps = extract_maps(input_lines)
 
     min_location = 9999999999
-    for s in actual_seeds:
-        location = calculate_end_location_for_seed(maps, s)
-        if location < min_location:
-            min_location = location
+    for seed_index, s in enumerate(seeds):
+        r = int(ranges[seed_index])
+        for i in range(0, r):
+            actual_seed = int(s) + i
+            location = calculate_end_location_for_seed(maps, actual_seed)
+            if location < min_location:
+                min_location = location
 
     print(f'result: {min_location}')
     return min_location
@@ -95,14 +65,27 @@ def calculate_end_location_for_seed(maps, s):
     return location
 
 
+def extract_maps(input_lines: list):
+    maps = [[], [], [], [], [], [], []]
+    map_idx = -1
+    for line in input_lines[1:]:
+        if line.__contains__('map'):
+            map_idx += 1
+        else:
+            numbers = re.findall(r'\d+', line)
+            if numbers:
+                maps[map_idx].append(numbers)
+    return maps
+
+
 if __name__ == '__main__':
     day = 5
     debug = False
     test_input_file = f'input/day{day}/testInput.txt'
     input_file = f'input/day{day}/input.txt'
 
-    # assert 35 == algo_part_one(test_input_file)
-    # assert 3374647 == algo_part_one(input_file)
+    assert 35 == algo_part_one(test_input_file)
+    assert 3374647 == algo_part_one(input_file)
 
     # part 2
     assert 46 == algo_part_two(test_input_file)
