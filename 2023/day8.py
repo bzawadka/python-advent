2a31c4beea065d092e47b66952617a4ba23ce3fc
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-12-08 11:17:16 +0100

    day 8 part 2 with cleanup

--- 2023/day8.py
+++ 2023/day8.py
@@ -1,4 +1,5 @@
 import re
+from math import lcm
 
 from common import read_file_to_list
 
@@ -9,35 +10,31 @@ def algo_part_one(input_file_name: str) -> int:
     lines = read_file_to_list(input_file_name)
 
     # read network map/dict
-    network_left = {}
-    network_right = {}
-    network_starting_node = ""
+    directions_left = {}
+    directions_right = {}
     for idx, line in enumerate(lines[2:]):
         (key, left, right) = re.findall(r'\w+', line)
-        network_left[key] = left
-        network_right[key] = right
-        if idx == 0:
-            network_starting_node = key
+        directions_left[key] = left
+        directions_right[key] = right
 
     # navigate using directions
-    directions = lines[0]
-    current_node = 'AAA'
+    instructions = lines[0]
 
     steps = 0
+    current_node = 'AAA'
     while current_node != 'ZZZ':
-        for d in directions:
+        for i in instructions:
             steps += 1
-            match d:
+            match i:
                 case 'L':
-                    current_node = network_left[current_node]
+                    current_node = directions_left[current_node]
                 case 'R':
-                    current_node = network_right[current_node]
+                    current_node = directions_right[current_node]
             if current_node == 'ZZZ':
                 break
 
-    result = steps
-    print(f'result: {result}')
-    return result
+    print(f'result: {steps}')
+    return steps
 
 
 # What if the map isn't for people - what if the map is for ghosts
@@ -49,55 +46,54 @@ def algo_part_two(input_file_name: str) -> int:
     lines = read_file_to_list(input_file_name)
 
     # read network map/dict
-    network_left = {}
-    network_right = {}
+    directions_left = {}
+    directions_right = {}
     network_starting_nodes = []
     for idx, line in enumerate(lines[2:]):
-        (key, left, right) = re.findall(r'\w+', line)
-        network_left[key] = left
-        network_right[key] = right
-        if key[2] == 'A':
+        (k, left, right) = re.findall(r'\w+', line)
+        key = str(k)
+        directions_left[key] = str(left)
+        directions_right[key] = str(right)
+        if key.endswith('A'):
             network_starting_nodes.append(key)
 
-    print(f'there are special nodes {network_starting_nodes}')
+    print(f'network starting nodes: {network_starting_nodes}')
 
     # navigate using directions
-    directions = lines[0]
-    current_nodes = network_starting_nodes
+    instructions = lines[0]
 
     steps = 0
-    while not all_current_nodes_end_with_z(current_nodes):
-        for d in directions:
+    results = {}
+    current_nodes = network_starting_nodes
+    while len(results) != len(network_starting_nodes):
+        for i in instructions:
             steps += 1
-            match d:
+            match i:
                 case 'L':
                     tmp = []
                     for cn in current_nodes:
-                        cn = network_left[cn]
+                        cn = directions_left[cn]
                         tmp.append(cn)
+                        if cn.endswith('Z'):
+                            results[cn] = steps
                     current_nodes = tmp
-                    # print(f'L -> {current_nodes}')
                 case 'R':
                     tmp = []
                     for cn in current_nodes:
-                        cn = network_right[cn]
+                        cn = directions_right[cn]
                         tmp.append(cn)
+                        if cn.endswith('Z'):
+                            results[cn] = steps
                     current_nodes = tmp
-                    # print(f'R -> {current_nodes}')
 
-            if all_current_nodes_end_with_z(current_nodes):
+            if len(results.keys()) == len(network_starting_nodes):
                 break
 
-    result = steps
-    print(f'result: {result}')
-    return result
-
-
-def all_current_nodes_end_with_z(nodes) -> bool:
-    for n in nodes:
-        if n[2] != 'Z':
-            return False
-    return True
+    print(f'network ending nodes, with the distance: {results}')
+    distances = [v for v in results.values()]
+    far_away_distance_where_they_will_meet = lcm(*distances)
+    print(f'result: {far_away_distance_where_they_will_meet}')
+    return far_away_distance_where_they_will_meet
 
 
 if __name__ == '__main__':
@@ -108,11 +104,10 @@ if __name__ == '__main__':
     input_file = f'input/day{day}/input.txt'
     test_input_file_part2 = f'input/day{day}/testInputPart2.txt'
 
-    # assert 6 == algo_part_one(test_input_file)
-    # assert 2 == algo_part_one(test_input_file2)
-    # assert 19637 == algo_part_one(input_file)
+    assert 6 == algo_part_one(test_input_file)
+    assert 2 == algo_part_one(test_input_file2)
+    assert 19637 == algo_part_one(input_file)
 
     # part 2
-    # assert 6 == algo_part_two(test_input_file_part2)
-    algo_part_two(input_file)
-    #     assert 42 ==
+    assert 6 == algo_part_two(test_input_file_part2)
+    assert 8811050362409 == algo_part_two(input_file)
