3afd5919ac51904dd8f2d988745b836de4bea8e4
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-12-09 08:12:40 +0100

    day 9 part 2

--- 2023/day9.py
+++ 2023/day9.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from common import read_file_to_list
 
 
@@ -12,7 +14,7 @@ def algo_part_one(input_file_name: str) -> int:
 
     for line in lines:
         # Phase 1: calculate last number in each sub-sequence: 0, 1, 5, 15
-        last_numbers = find_last_numbers_for(line)
+        last_numbers = find_numbers_for(line, WhichNumber.LAST)
 
         # Phase 2: calculate new numbers: 0, 1, 7, 28
         # 1   3   6  10  15  21  28
@@ -20,12 +22,11 @@ def algo_part_one(input_file_name: str) -> int:
         #     1   1   1   1   1
         #       0   0   0   0
         new_numbers = [0]
-        rev = list(reversed(last_numbers))
-        for idx, n in enumerate(rev):
+        for idx, n in enumerate(reversed(last_numbers)):
             if idx == 0:
                 continue
-            n = n + new_numbers[idx - 1]
-            new_numbers.append(n)
+            new_n = n + new_numbers[idx - 1]
+            new_numbers.append(new_n)
 
         # Phase 3: add 28 to the result
         last_one = new_numbers.pop()
@@ -36,19 +37,30 @@ def algo_part_one(input_file_name: str) -> int:
     return result
 
 
-def find_last_numbers_for(line: str) -> list[int]:
+class WhichNumber(Enum):
+    FIRST = 1
+    LAST = 2
+
+
+def find_numbers_for(line: str, first_or_last: WhichNumber) -> list[int]:
     # 1 3 6 10 15 21
-    last_numbers = []
+    numbers = []
     # Phase 1: calculate last number in each sub-sequence: 0, 1, 5, 15
     # 1   3   6  10  15  21
     #   2   3   4   5   6
     #     1   1   1   1
     #       0   0   0
     current_line = line
-    while not all_chars_are_zeros(current_line):
+    while not all_chars_are_zeros_in(current_line):
         current_l = current_line.split(' ')
-        last_number = int(current_l[-1])
-        last_numbers.append(last_number)
+        match first_or_last:
+            case WhichNumber.FIRST:
+                first_number = int(current_l[0])
+                numbers.append(first_number)
+            case WhichNumber.LAST:
+                last_number = int(current_l[-1])
+                numbers.append(last_number)
+
         tmp_line = ''
         for idx, num in enumerate(current_l):
             if idx == len(current_l) - 1:
@@ -60,19 +72,36 @@ def find_last_numbers_for(line: str) -> list[int]:
 
         current_line = tmp_line
 
-    last_numbers.append(0)
-    return last_numbers
+    numbers.append(0)
+    return numbers
 
 
-def all_chars_are_zeros(chars: str) -> bool:
-    if len(set(chars)) == 2 and ('0' in set(chars) and ' ' in set(chars)):
+def all_chars_are_zeros_in(s: str) -> bool:
+    if len(set(s)) == 2 and ('0' in set(s) and ' ' in set(s)):
         return True
     return False
 
 
 def algo_part_two(input_file_name: str) -> int:
     print("running algo part two..." + input_file_name)
-    return algo_part_one(input_file_name)
+    lines = read_file_to_list(input_file_name)
+
+    result = 0
+    for line in lines:
+        first_numbers = find_numbers_for(line, WhichNumber.FIRST)
+
+        new_numbers = [0]
+        for idx, n in enumerate(reversed(first_numbers)):
+            if idx == 0:
+                continue
+            new_n = n - new_numbers[idx - 1]
+            new_numbers.append(new_n)
+
+        result += new_numbers.pop()
+
+    # What is the sum of extrapolated values?
+    print(f'result: {result}')
+    return result
 
 
 if __name__ == '__main__':
@@ -85,5 +114,5 @@ if __name__ == '__main__':
     assert 1581679977 == algo_part_one(input_file)
 
     # part 2
-    # assert 42 == algo_part_two(test_input_file)
-    # assert 42 == algo_part_two(input_file)
+    assert 2 == algo_part_two(test_input_file)
+    assert 889 == algo_part_two(input_file)
