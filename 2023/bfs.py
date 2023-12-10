246aadff2074a47c6fb367832e001f24b216a426
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-12-10 07:09:21 +0100

    day 10 part 1

---
+++ 2023/bfs.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, val: int):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def __repr__(self):
+        return str(self.val)
+
+    def insert_node(self, val: int):
+        if self.val is None:
+            return
+
+        if val < self.val:
+            if self.left is None:
+                self.left = Node(val)
+            else:
+                self.left.insert_node(val)
+        else:  # val > self.val OR val is the same
+            if self.right is None:
+                self.right = Node(val)
+            else:
+                self.right.insert_node(val)
+
+    @staticmethod
+    def insert_nodes(vals: list[int], root):
+        [root.insert_node(i) for i in vals]
+
+    def bfs(self, root=None):
+        if root is None:
+            return
+        queue = [root]
+
+        while len(queue) > 0:
+            cur_node = queue.pop(0)
+
+            if cur_node.left is not None:
+                queue.append(cur_node.left)
+
+            if cur_node.right is not None:
+                queue.append(cur_node.right)
+
+            print(queue)
+
+
+if __name__ == '__main__':
+    print('bfs')
+    the_root = Node(4)
+    the_root.insert_nodes([2, 1, 3, 6, 5, 7], the_root)
+    print(the_root)
+    the_root.bfs(root=the_root)
--- 2023/day10.py
+++ 2023/day10.py
@@ -1,18 +1,76 @@
-import re
+from enum import Enum
 
 from common import read_file_to_list
 
 
-def algo_part_one(input_file_name: str) -> int:
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    EAST = 3
+    WEST = 4
+
+
+# --- Day 10: Pipe Maze ---
+# Find the single giant loop starting at S
+# How many steps along the loop does it take to get
+# from the starting position to the point farthest from the starting position?
+def algo_part_one(input_file_name: str, s_x: int, s_y: int,
+                  direction_a: Direction, direction_b: Direction) -> int:
     print("running algo part one..." + input_file_name)
-    lines = read_file_to_list(input_file_name)
+    the_map = read_file_to_list(input_file_name)
 
-    for line in lines:
-        print(line)
+    print(f'starting at: {the_map[s_x][s_y]}')
+    ax = s_x
+    ay = s_y
+    bx = s_x
+    by = s_y
+    distance = 0
+    while distance < 1000000:
+        distance += 1
+        # make the move - both A and B
+        # result is new coordinates and new direction
+        # check if A and B are met,
+        #   if so: stop the loop
+        (ax, ay, direction_a) = make_the_move(ax, ay, direction_a, the_map)
+        (bx, by, direction_b) = make_the_move(bx, by, direction_b, the_map)
+        if ax == bx and ay == by:
+            break
 
-    result = 42
-    print(f'result: {result}')
-    return result
+    print(f'result: {distance}')
+    return distance
+
+
+def make_the_move(x: int, y: int, direction: Direction, the_map: list[str]) -> (int, int, Direction):
+    match direction:
+        case Direction.NORTH:
+            x -= 1
+        case Direction.EAST:
+            y += 1
+        case Direction.SOUTH:
+            x += 1
+        case Direction.WEST:
+            y -= 1
+
+    next_map_piece = the_map[x][y]
+    match next_map_piece:
+        case 'J':
+            direction = Direction.NORTH if direction == Direction.EAST else Direction.WEST
+        case 'F':
+            direction = Direction.EAST if direction == Direction.NORTH else Direction.SOUTH
+        case '7':
+            direction = Direction.SOUTH if direction == Direction.EAST else Direction.WEST
+        case 'L':
+            direction = Direction.EAST if direction == Direction.SOUTH else Direction.NORTH
+        case '-':
+            direction = direction
+        case '|':
+            direction = direction
+            # unchanged, it must have been north or south already
+            # direction = Direction.NORTH if direction == Direction.NORTH else Direction.SOUTH
+        case _:
+            print(f'unknown direction: {next_map_piece}')
+
+    return x, y, direction
 
 
 def algo_part_two(input_file_name: str) -> int:
@@ -26,8 +84,8 @@ if __name__ == '__main__':
     test_input_file = f'input/day{day}/testInput.txt'
     input_file = f'input/day{day}/input.txt'
 
-    assert 42 == algo_part_one(test_input_file)
-    # assert 42 == algo_part_one(input_file)
+    assert 8 == algo_part_one(test_input_file, 2, 0, Direction.EAST, Direction.SOUTH)
+    algo_part_one(input_file, 96, 101, Direction.EAST, Direction.NORTH)
 
     # part 2
     # assert 42 == algo_part_two(test_input_file)
