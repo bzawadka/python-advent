6d15aaec11f518ea2e7ef174743b1b8d47d434c7
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-12-13 23:18:24 +0100

    prep

---
+++ 2023/day14.py
@@ -0,0 +1,34 @@
+import re
+
+from common import read_file_to_list
+
+
+def algo_part_one(input_file_name: str) -> int:
+    print("running algo part one..." + input_file_name)
+    lines = read_file_to_list(input_file_name)
+
+    for line in lines:
+        print(line)
+
+    result = 42
+    print(f'result: {result}')
+    return result
+
+
+def algo_part_two(input_file_name: str) -> int:
+    print("running algo part two..." + input_file_name)
+    return algo_part_one(input_file_name)
+
+
+if __name__ == '__main__':
+    day = 14
+    debug = False
+    test_input_file = f'input/day{day}/testInput.txt'
+    input_file = f'input/day{day}/input.txt'
+
+    assert 42 == algo_part_one(test_input_file)
+    # assert 42 == algo_part_one(input_file)
+
+    # part 2
+    # assert 42 == algo_part_two(test_input_file)
+    # assert 42 == algo_part_two(input_file)
